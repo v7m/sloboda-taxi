9e626a59565d3b77839665a84d62eb3a1dda2c6b
v7m <deathside7@gmail.com>
2015-08-14 16:02:31 +0300

    Add status :opened to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   belongs_to :client, class_name: "User"
   belongs_to :driver, class_name: "User"
 
-  enum status: [:closed, :pending, :confirmed, :edited, :rejected]
+  enum status: [:closed, :pending, :confirmed, :edited, :rejected, :opened]
   enum car_type: [:sedan, :minivan, :truck]
 
 end
