a1d6c2b46e5556d90bb56b6cac8403384ef4e05f
v7m <deathside7@gmail.com>
2015-08-16 20:23:24 +0300

    Fix binding bug for driver view

--- app/assets/javascripts/websocket.js
+++ app/assets/javascripts/websocket.js
@@ -23,63 +23,86 @@ function statusContent(order) {
 }
 
 function changeStatusInAllForDispatcher(order){
-  return $(".dispatcher_index[data-orders-status='all'] #dispatcher_table > .order_" + order.id + " > .status").html(statusContent(order));
+  $(".dispatcher_index[data-orders-status='all'] #dispatcher_table > .order_" + order.id + " > .status").html(statusContent(order));
 }
 
 function removeOrderForDispatcher(order, status){
-  return $(".dispatcher_index[data-orders-status='" + status + "'] #dispatcher_table > .order_" + order.id).hide("slow", function(){
-        $(this).remove();
-      });
+  $(".dispatcher_index[data-orders-status='" + status + "'] #dispatcher_table > .order_" + order.id).hide("slow", function(){
+    $(this).remove();
+  });
 }
 
-function addOrderToDispatcher(order, status){
-  return $(".dispatcher_index[data-orders-status='" + status + "'] #dispatcher_table").prepend(orderContent(order))
+
+function addOrderForDispatcher(order, status){
+  $(".dispatcher_index[data-orders-status='" + status + "'] #dispatcher_table").prepend(orderContent(order));
+}
+
+function changeStatusInAllForDriver(order){
+  $(".driver_index[data-orders-status='all'] #driver_" + order.driver_id + "_table > .order_" + order.id + " > .status").html(statusContent(order));
+}
+
+function addOrderForDriver(order, status){
+  $(".driver_index[data-orders-status='" + status + "'] #driver_" + order.driver_id + "_table").prepend(orderContent(order));
+}
+
+function removeOrderForDriver(order, status){
+  $(".driver_index[data-orders-status='" + status + "'] #driver_" + order.driver_id + "_table > .order_" + order.id).hide("slow", function(){
+    $(this).remove();
+  });
 }
 
 channel.bind('new', function(order) {
-  addOrderToDispatcher(order, "all");
-  addOrderToDispatcher(order, "opened");
+  addOrderForDispatcher(order, "all");
+  addOrderForDispatcher(order, "opened");
 });
 
 channel.bind('assign_driver', function(order) {
-    if( $("#driver_" + order.driver_id + "_table > .order_" + order.id).length) { 
-
-        $("#driver_" + order.driver_id + "_table > .order_" + order.id + " > .status").html(statusContent(order));  
-        
-    } else {
-       $("#driver_" + order.driver_id + "_table").prepend(orderContent(order));
-    }
-
-    changeStatusInAllForDispatcher(order);
-    removeOrderForDispatcher(order, "opened");
-    addOrderToDispatcher(order, "pending");
+  addOrderForDriver(order, "all");
+  addOrderForDriver(order, "pending");
+  changeStatusInAllForDispatcher(order);
+  removeOrderForDispatcher(order, "rejected");
+  removeOrderForDispatcher(order, "opened");
+  addOrderForDispatcher(order, "pending");
 });
 
 channel.bind('confirm', function(order) {
   changeStatusInAllForDispatcher(order);
   removeOrderForDispatcher(order, "pending");
-  addOrderToDispatcher(order, "confirmed");
+  addOrderForDispatcher(order, "confirmed");
 });
 
 channel.bind('close', function(order) {
   changeStatusInAllForDispatcher(order);
   removeOrderForDispatcher(order, "confirmed");
-  addOrderToDispatcher(order, "closed");
+  addOrderForDispatcher(order, "closed");
 });
 
 channel.bind('reject', function(order) {
-  $("#driver_" + order.driver_id + "_table > .order_" + order.id).hide("slow", function(){
-    $(this).remove();
-  });
+  changeStatusInAllForDispatcher(order);
+  removeOrderForDispatcher(order, "pending");
+  removeOrderForDispatcher(order, "edited");
+  addOrderForDispatcher(order, "rejected");
+  removeOrderForDriver(order, "all");
+  removeOrderForDriver(order, "pending");
+  removeOrderForDriver(order, "edited");
 });
 
 channel.bind('change', function(order) {
+  changeStatusInAllForDriver(order);
+  removeOrderForDriver(order, "pending");
+  removeOrderForDriver(order, "closed");
+  addOrderForDriver(order, "edited");
   changeStatusInAllForDispatcher(order);
   removeOrderForDispatcher(order, "pending");
   removeOrderForDispatcher(order, "closed");
-  addOrderToDispatcher(order, "edited");
+  addOrderForDispatcher(order, "edited");
 });
 
 channel.bind('accept_changes', function(order) {
-  $("#driver_" + order.driver_id + "_table > .order_" + order.id + " > .status").html(statusContent(order));
+  changeStatusInAllForDriver(order);
+  removeOrderForDriver(order, "edited");
+  addOrderForDriver(order, "pending");
+  changeStatusInAllForDispatcher(order);
+  removeOrderForDispatcher(order, "edited");
+  addOrderForDispatcher(order, "pending");
 });
