d715199fd478da84cb9d57871957296de2b50582
v7m <deathside7@gmail.com>
2015-08-17 14:14:54 +0300

    Create routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,8 @@ Rails.application.routes.draw do
       put :add_feedback 
       put :accept_changes
     end
-  end    
+  end 
+  resources :users, only: [:index, :show]   
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
