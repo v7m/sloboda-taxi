c98f01952d6829ca7f112b867c99a182102c1d32
v7m <deathside7@gmail.com>
2015-08-17 14:59:34 +0300

    Add drivers sorting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @drivers = User.with_role(:driver)
+    params[:car_type] ? @car_type = params[:car_type] : @car_type = "all"
+      if @car_type == "all" 
+        @drivers = User.with_role(:driver)
+      else  
+        @drivers = User.with_role(:driver).with_car_type(@car_type)
+      end  
+    respond_to do |format|
+      format.html 
+      format.js { render 'sort_index' }
+    end  
   end
 
 end
