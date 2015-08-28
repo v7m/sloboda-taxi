484b416cacc9b8be07eeb82784cef923745d67e3
v7m <deathside7@gmail.com>
2015-08-28 13:09:38 +0300

    Fix assigning driver with non closed order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,11 +12,11 @@ class OrdersController < ApplicationController
       format.js { render 'sort_index' }
     end
   end
-  
+
   def show
     authorize! :read, Order
     session[:return_to] ||= request.referer
-    @drivers =  User.with_role(:driver).with_car_type(@order.car_type)
+    @drivers =  User.with_role(:driver).with_car_type(@order.car_type).not_busy
   end
 
   def new
@@ -40,11 +40,13 @@ class OrdersController < ApplicationController
   def edit_driver
     authorize! :assign_driver, Order
     @drivers =  User.with_role(:driver)
-  end  
+  end
 
   def assign_driver
     authorize! :assign_driver, Order
     if @order.update(params[:order].permit(:driver_id, :status))
+      driver = @order.driver
+      driver.set_busyness(true)
       @order.websocket_trigger(:assign_driver)
       flash[:notice] = "Driver successfully assigned"
       respond_to do |format|
@@ -67,7 +69,7 @@ class OrdersController < ApplicationController
       @order.notify_about_confirm
     else
       render :show  
-    end  
+    end
   end
 
   def edit
@@ -95,12 +97,14 @@ class OrdersController < ApplicationController
       end
     else
       render :show  
-    end  
+    end
   end
 
   def close
     authorize! :close, Order
     if @order.closed!
+      driver = @order.driver
+      driver.set_busyness(false)
       @order.websocket_trigger(:close)
       respond_to do |format|
         format.html { redirect_to order_path(@order) }
@@ -108,14 +112,16 @@ class OrdersController < ApplicationController
       end
       @order.notify_about_close
     else
-      render :show    
-    end  
+      render :show
+    end
   end
 
   def reject
     authorize! :reject, Order
     @drivers =  User.with_role(:driver).with_car_type(@order.car_type)
     if @order.rejected!
+      driver = @order.driver
+      driver.set_busyness(false)
       @order.websocket_trigger(:reject)
       flash[:notice] = "Order successfully rejected"
       respond_to do |format|
@@ -126,7 +132,7 @@ class OrdersController < ApplicationController
     else
       render :show  
     end
-  end  
+  end
 
   def add_feedback
     authorize! :add_feedback, Order
@@ -141,8 +147,8 @@ class OrdersController < ApplicationController
         format.html { render action: "show" }
         format.js { render :feedback_errors }
       end
-    end  
-  end  
+    end
+  end
 
   def destroy_feedback
     authorize! :destroy_feedback, Order
@@ -154,14 +160,14 @@ class OrdersController < ApplicationController
       end
     else
       render action: "show"
-    end  
-  end  
+    end
+  end
 
   private
 
   def get_order
     @order = Order.find(params[:id])
-  end  
+  end
 
   def order_params
     params.require(:order).permit(:departure, :destination, :datetime, :car_type, :status)
@@ -169,8 +175,6 @@ class OrdersController < ApplicationController
 
   def clean_order_driver
     @order.update_attributes(driver_id: nil)
-  end  
-
-
+  end
 
 end
